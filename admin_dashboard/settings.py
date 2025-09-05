046053d3e51cab2ffac16b612e9a43bdccfeee77
TMeghaHarsha <thotameghasaiharsha@gmail.com>
2025-09-05 09:22:10 +0530

    Update DATABASES to use db hostname instead of localhost

--- admin_dashboard/settings.py
+++ admin_dashboard/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'admin_dashboard.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {'default': {'ENGINE': 'django.db.backends.postgresql', 'NAME': 'farming_db', 'USER': 'postgres', 'PASSWORD': 'lab@123', 'HOST': 'localhost', 'PORT': '5432'}}
+DATABASES = {'default': {'ENGINE': 'django.db.backends.postgresql', 'NAME': 'farming_db', 'USER': 'postgres', 'PASSWORD': 'lab@123', 'HOST': 'db', 'PORT': '5432'}}
 
 CACHES = {'default': {'BACKEND': 'django_redis.cache.RedisCache', 'LOCATION': 'redis://127.0.0.1:6379/1'}}
 
